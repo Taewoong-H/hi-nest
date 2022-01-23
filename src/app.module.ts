fa917f46a72aae8224f804a6a2897447e7921e56
Taewoong-H <taeung2008@gmail.com>
2022-01-23 22:12:52 +0900

    feat: CRUD API, verfying with dto

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { MoviesController } from './movies/movies.controller';
+import { MoviesService } from './movies/movies.service';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [MoviesController], //url을 가져오고, 함수를 실행함(like router)
+  providers: [MoviesService], // 여기에서 함수들을 정의함. 데이터베이스 쿼리도 여기서 다룸
 })
 export class AppModule {}
